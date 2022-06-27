0aaf7358b603701987c1fd75ecad3517af1244b9
dlatldhs <sionzz1713@gmail.com>
2022-06-27 21:28:52 +0900

    약간 되는지 안되는지 모르겠는데 이론상 가능

--- src/components/blackJack.js
+++ src/components/blackJack.js
@@ -1,4 +1,4 @@
-class Cards extends React.Component { // react component 사용
+class blackJack extends React.Component { // react component 사용
     constructor(props){               // React component props method init & setting
         super(props);                 // 이거 해야 this 사용 가능
 
@@ -26,5 +26,19 @@ class Cards extends React.Component { // react component 사용
             }
         }
         return deck
+    }
+
+    // 랜덤 카드 반환 하는 함수
+    getRandomCard(deck) {
+        const updatedDeck = deck;  // 위에 덱 만들어놓은거
+        const randomIndex = Math.floor(Math.random() * updatedDeck.length ); // random 난수 생성
+        const randomCard = updatedDeck[randomIndex] 
+        updatedDeck.splice(randomIndex,1);
+        return { randomCard , updatedDeck }
+    }
+
+    // 딜러 & 플레이어의 카드를 실질적으로 만듬
+    dealCard(deck)  {
+         
     }
 }
