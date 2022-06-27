3108082df9581794ddb7fd31b19b0a4a82c3511e
dlatldhs <sionzz1713@gmail.com>
2022-06-27 20:06:18 +0900

    first commit ( making header )

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+const Insert = () => {
+    return (
+        <header className="header">
+            <h1 className="header-title">BLACK JACK</h1>
+        </header>
+    )
+
+};
+
+export default Insert;
